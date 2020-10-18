7af541f10525bfc02ab4c3b2ed9ac0531041f4af
Fritkot99 <36574860+Fritkot99@users.noreply.github.com>
2020-10-18 12:54:20 +0200

    Update mainAddon2.py

--- mainAddon2.py
+++ mainAddon2.py
@@ -21,7 +21,7 @@ bl_info = {
     "name": "Airfoil Tool",
     "description": "Generate airfoil surface from Naca code or file",
     "author": "Fritkot99",
-    "version":(0,4),
+    "version":(0,5),
     "blender": (2,90,1),
     }
 
@@ -305,11 +305,15 @@ class GenerateSurface(bpy.types.Operator):
         global chordLength
         global meshname
         global planeDraw
+        global chordDir
+        global thickDir
         
         
+        #make variables to mutliply coordinates with for orientation
+        Xinv = -2*int(chordDir)+1
+        Yinv = -2*int(thickDir)+1
         
-            
-        
+        #print("inversionstuff", Xinv, Yinv)
         
         PtsNew = Pts*chordLength
    
@@ -328,13 +332,13 @@ class GenerateSurface(bpy.types.Operator):
         i = 0
         while run:
             if planeDraw == 'XY':
-                verts1.append(Vector((PtsNew[i][0], PtsNew[i][1], 0)))
+                verts1.append(Vector((Xinv*PtsNew[i][0], Yinv*PtsNew[i][1], 0)))
             
             elif planeDraw == 'XZ':
-                verts1.append(Vector((PtsNew[i][0], 0,PtsNew[i][1])))
+                verts1.append(Vector((Xinv*PtsNew[i][0], 0,Yinv*PtsNew[i][1])))
                 
             elif planeDraw == 'YZ':
-                verts1.append(Vector((0, PtsNew[i][0],PtsNew[i][1])))
+                verts1.append(Vector((0, Xinv*PtsNew[i][0],Yinv*PtsNew[i][1])))
                 
                 
             i+=1
@@ -621,6 +625,7 @@ class airfoilClassPanel(Panel):
         
 
 class airfoilClassPanel2(Panel):
+    """class to draw the plotting options of the panel"""
     bpy.types.Scene.naca_or_file = True
     #obj = context.object
     bl_label = "Geometry"
@@ -645,6 +650,13 @@ class airfoilClassPanel2(Panel):
     bpy.types.Scene.faceornot= BoolProperty(name = "Fill surface",
                                             description = "If selected, the airfoil outline will be filled to make a plane",
                                             default = True)
+                                            
+    bpy.types.Scene.chordinv= BoolProperty(name = "Chord",
+                                            description = "This will draw the chord length in the negative direction of the chord axis",
+                                            default = False)
+    bpy.types.Scene.heightinv= BoolProperty(name = "Thickness",
+                                            description = "This will draw the airfoil thickness in the negative direction of the height axis",
+                                            default = False)
                                                                                                                             
     
     
@@ -677,6 +689,9 @@ class airfoilClassPanel2(Panel):
         global chordLength
         global planeDraw
         
+        global chordDir
+        global thickDir
+        
         #nacacamber2 = self.nacacamber
         
         layout = self.layout
@@ -697,6 +712,11 @@ class airfoilClassPanel2(Panel):
         
         row.prop(context.scene, "planeselect")
         row = layout.row()
+        row.label(text="Reverse orientation:")
+        row = layout.row()
+        row.prop(context.scene, "chordinv")
+        row.prop(context.scene, "heightinv")
+        row = layout.row()
         row.operator("ops.generate_surface")
         
         
@@ -704,6 +724,8 @@ class airfoilClassPanel2(Panel):
         Faceornot = bpy.context.scene.faceornot
         chordLength = bpy.context.scene.chordlength
         planeDraw = bpy.context.scene.planeselect
+        chordDir = bpy.context.scene.chordinv
+        thickDir = bpy.context.scene.heightinv
    
     
 classes = [airfoilClassPanel,
